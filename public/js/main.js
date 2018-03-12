a71aff29c1111800bd97ca0deb2a778fc0a07caf
Bayley <cbayley@proforma.com>
2018-03-12 16:33:20 -0400

    removed forms and added more button refs

--- public/js/main.js
+++ public/js/main.js
@@ -15,6 +15,12 @@ const team2NameInput = team2TeamBox.querySelector('.team-name-input');
 const team1NameUpdateButton = team1TeamBox.querySelector('.team-name-button');
 const team2NameUpdateButton = team2TeamBox.querySelector('.team-name-button');
 
+const team1NameAddButton = team1TeamBox.querySelector('.team-score-inc-button');
+const team2NameAddButton = team2TeamBox.querySelector('.team-score-inc-button');
+
+const team1NameSubButton = team1TeamBox.querySelector('.team-score-dec-button');
+const team2NameSubButton = team2TeamBox.querySelector('.team-score-dec-button');
+
 // Output related
 const team1NameHeader = team1TeamBox.querySelector('.team-name');
 const team2NameHeader = team2TeamBox.querySelector('.team-name');
@@ -24,12 +30,10 @@ const team2ScoreHeader = team2TeamBox.querySelector('.team-score');
 
 const main = () => {
   team1NameUpdateButton.addEventListener('click', (event) => {
-    event.preventDefault();
     team1Name = team1NameInput.value;
     team1NameHeader.textContent = team1Name;
   });
   team2NameUpdateButton.addEventListener('click', (event) => {
-    event.preventDefault();
     team2Name = team2NameInput.value;
     team2NameHeader.textContent = team2Name;
   });
