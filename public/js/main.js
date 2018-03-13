c770f9ead1c21ac238af3e52cd46b06d6159c3f8
Bayley <cbayley@proforma.com>
2018-03-13 10:07:34 -0400

    Finished adventure * I need to start commiting more often

--- public/js/main.js
+++ public/js/main.js
@@ -27,13 +27,56 @@ class Team {
   }
 }
 
-const teamCount = 2;
+const teamCount = 2; // Config
+const periodCount = 2; // Config
 const teams = [];
+const periods = [];
+let currentPeriod = 1;
+let gameOver = false;
 
 const main = () => {
   for (let i = 1; i < teamCount + 1; i++) {
     teams.push(new Team(`Team ${i}`, `#team${i}TeamBox`));
   }
+  
+  document.querySelector('.next-period-button').addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(periods);
+    if (gameOver) {
+      teams.forEach((team) => {
+        team.score = 0;
+      });
+      periods.length = 0;
+      currentPeriod = 1;
+      gameOver = false;
+      document.querySelector('.period').textContent = 1;
+      display = document.querySelector('.periods-display');
+      while (display.firstChild) {
+        display.removeChild(display.firstChild);
+      }
+      return;
+    }
+
+    const period = [];
+    teams.forEach((team) => {
+      period.push(team.score);
+    });
+    periods.push(period);
+    
+    if (currentPeriod == periodCount) {
+      gameOver = true;
+      document.querySelector('.period').textContent = 'Game Over!';
+    }
+    else {
+      currentPeriod += 1;
+      document.querySelector('.period').textContent = currentPeriod;
+    }
+
+    paragraph = document.createElement('p');
+    paragraph.textContent = period;
+    display = document.querySelector('.periods-display');
+    display.appendChild(paragraph);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', main);
